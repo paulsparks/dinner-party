a16acbb749dbab99b1202c868a774f4064a1810c
paulsparks <psparks1225@gmail.com>
2026-06-05 19:52:16 -0400

    devenv

---
+++ src/app/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+    primaryColor: "orange",
+    colors: {
+        orange: [
+            "#fffaf7",
+            "#fff2e8",
+            "#ffdfc4",
+            "#ffc49a",
+            "#ffa06b",
+            "#f07c3e",
+            "#d45e1e",
+            "#b04415",
+            "#8c300d",
+            "#6b1f07",
+        ],
+    },
+});
