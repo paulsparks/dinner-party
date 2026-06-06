cd631730aebd40c605742ca6f8fda002d413a52e
paulsparks <psparks1225@gmail.com>
2026-06-06 15:17:23 -0400

    pull user info

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import { useAuthenticatedContext } from "@/contexts/AuthenticatedContext";
+
 export default function Home() {
+    const { user } = useAuthenticatedContext();
+
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-7xl">James and Paul Dinner Party</h1>
             <p className="text-4xl">home page</p>
+            <p>Email: {user.email}</p>
+            <p>Name: {user.name}</p>
         </div>
     );
 }
